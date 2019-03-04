f7fbe4b517f14b0a311c3c9743cdc05bad86a6be
Oliver <ors@oliver-stott.co.uk>
2019-03-04 18:25:32 +0000

    Created list item functionality, add, delete and update list items. Added an API called uniqe, which generates a unique ID easily on an array item. Tested and working okay. Data model has been created and added to the controller. Next I will be sorting the View.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -102,7 +103,7 @@ if (e.target.matches('.btn-decrease, .btn-decrease *')) {
         //Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
-
     }
-console.log(state.recipe);
 });
+
+window.l = new List();
